2034e8441b3a60911189a25c3411659793c604bc
robert8326 <robertwerlko@gmail.com>
2021-08-12 12:05:00 +0500

    Добавил send_message в footer

--- contact/views.py
+++ contact/views.py
@@ -3,9 +3,18 @@ from django.views.generic import CreateView
 
 from contact.forms import ContactForm
 from contact.models import Contact
+from movies.service import send
 
 
 class ContactView(CreateView):
     model = Contact
     form_class = ContactForm
     success_url = '/'
+    template_name = 'movies/movie_list.html'
+
+    def form_valid(self, form):
+        form.save()
+        send(form.instance.email)
+        print(form)
+        print(form.instance.email)
+        return super().form_valid(form)
--- movie_django/settings.py
+++ movie_django/settings.py
@@ -2,7 +2,6 @@ from pathlib import Path
 from environs import Env
 import os
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -55,7 +54,7 @@ ROOT_URLCONF = 'movie_django.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,6 +117,13 @@ STATIC_URL = '/static/'
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [STATIC_DIR]
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = env.str('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env.str('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
